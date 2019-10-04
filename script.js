a4ef4df28b588d19c80ad4d35cc764972f2640ba
Brooks Geenen <brgeen@gmail.com>
2019-10-04 16:33:45 -0500

    add calculateMonthlySalary function, establish global monthlySalary variable, .text monthlySalary to the DOM

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ function onReady() {
     console.log('jquery running');
     $('#add-employee-button').on('click', addEmployeeButton);
     $('table').on('click', '#delete-button', deleteEmployeeButton);
+    $('#total-monthly-salary').on('load', calculateMonthlySalary);
 
 }
+
+let monthlySalary = 100;
 
 function addEmployeeButton() {
     let firstName = $('#first-name-input').val();
@@ -24,17 +27,25 @@ function addEmployeeButton() {
         <td>${salary}</td>
         <td><button id="delete-button">Delete</button></td>
         </tr>`);
-    
+
     firstName = $('#first-name-input').val('');
     lastName = $('#last-name-input').val('');
     id = $('#id-input').val('');
     title = $('#title-input').val('');
     salary = $('#annual-salary-input').val('');
 
+    monthlySalary += 100;
+    calculateMonthlySalary();
 
 }
 
 function deleteEmployeeButton() {
     $(this).closest('tr').remove();
-    
+    monthlySalary -= 50;
+    calculateMonthlySalary();
+}
+
+
+function calculateMonthlySalary() {
+    $('#total-monthly-salary').text(monthlySalary)
 }

14a7e778eefda62acae6d171bf55971691b027b8
Brooks Geenen <brgeen@gmail.com>
2019-10-04 15:36:04 -0500

    create addEmployeeButton function that takes input, logs it to console, empties the fields

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery running');
-    
+    $('#add-employee-button').on('click', addEmployeeButton);
+
+}
+
+function addEmployeeButton() {
+    let firstName = $('#first-name-input').val();
+    let lastName = $('#last-name-input').val();
+    let id = $('#id-input').val();
+    let title = $('#title-input').val();
+    let salary = $('#annual-salary-input').val();
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(id);
+    console.log(title);
+    console.log(salary);
+
+
+    firstName = $('#first-name-input').val('');
+    lastName = $('#last-name-input').val('');
+    id = $('#id-input').val('');
+    title = $('#title-input').val('');
+    salary = $('#annual-salary-input').val('');
+
+
+
 }

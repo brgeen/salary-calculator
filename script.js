2667b65ab4a87a67f159d689356e0508e1941aa7
Brooks Geenen <brgeen@gmail.com>
2019-10-06 20:13:33 -0500

    added conditionals for no
    input from user

--- script.js
+++ script.js
@@ -18,7 +18,25 @@ function addEmployeeButton() {
     let title = $('#title-input').val();
     let salary = $('#annual-salary-input').val();
 
-    $('table').append(`<tr>
+    if (firstName.length === 0) {
+        $('#first-name-input').attr('placeholder', 'First Name*');
+    } else if (lastName.length === 0) {
+        $('#last-name-input').attr('placeholder', 'Last Name*');
+    } else if (id.length === 0) {
+        $('#id-input').attr('placeholder', 'ID*');
+    } else if (title.length === 0) {
+        $('#title-input').attr('placeholder', 'Title*');
+    } else if (salary.length === 0) {
+        $('#annual-salary-input').attr('placeholder', 'Annual Salary*');
+    } else {
+
+        $('#first-name-input').attr('placeholder', 'First Name');
+        $('#last-name-input').attr('placeholder', 'Last Name');
+        $('#id-input').attr('placeholder', 'ID');
+        $('#title-input').attr('placeholder', 'Title');
+        $('#annual-salary-input').attr('placeholder', 'Annual Salary');
+
+        $('table').append(`<tr>
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${id}</td>
@@ -27,16 +45,16 @@ function addEmployeeButton() {
         <td><button class="delete-button">Delete</button></td>
         </tr>`);
 
-    annualSalary += parseInt(salary);
+        annualSalary += parseInt(salary);
 
-    firstName = $('#first-name-input').val('');
-    lastName = $('#last-name-input').val('');
-    id = $('#id-input').val('');
-    title = $('#title-input').val('');
-    salary = $('#annual-salary-input').val('');
+        firstName = $('#first-name-input').val('');
+        lastName = $('#last-name-input').val('');
+        id = $('#id-input').val('');
+        title = $('#title-input').val('');
+        salary = $('#annual-salary-input').val('');
 
-    calculateMonthlySalary();
-
+        calculateMonthlySalary();
+    }
 }
 
 function deleteEmployeeButton() {
